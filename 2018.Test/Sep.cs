a5286664ef479de60c7b2ba917684bbac4bf5681
姜小骚 <826420016@qq.com>
2018-09-27 22:19:37 +0800

    Sep27 CheckIn

--- 2018.Test/Sep.cs
+++ 2018.Test/Sep.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using _2018.September;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace _2018.Test
 {
     [TestClass]
-    public class MainTestClass
+    public class Sep
     {
+        #region Sep26
         [TestMethod]
         public void Sep26Test_One()
         {
             var args = new int[] { 3, 5, 7, 12, 44, 69 };
             var target = 51;
-            var result = new Sep26().twoSum(args,target);
+            var result = new Sep26().twoSum(args, target);
             Assert.AreEqual(result[0], 2);
             Assert.AreEqual(result[1], 4);
         }
@@ -32,5 +34,26 @@ namespace _2018.Test
             var result = new Sep26().twoSum(args, target);
             Assert.IsNull(result);
         }
+        #endregion
+        #region Sep27
+        [TestMethod]
+        public void Sep27Test_One()
+        {
+            //Test Max & Min value
+            var input = int.MaxValue;
+            var result = Sep27.Reverse(input);
+            Assert.AreEqual(result, 0);
+            result = Sep27.Reverse(int.MinValue);
+            Assert.AreEqual(result, 0);
+        }
+        [TestMethod]
+        public void Sep27Test_Two()
+        {
+            //Normal Test
+            Assert.AreEqual(Sep27.Reverse(123), 321);
+            Assert.AreEqual(Sep27.Reverse(-123), -321);
+            Assert.AreEqual(Sep27.Reverse(120), 21);
+        }
+        #endregion
     }
 }
---
+++ 2018/September/Sep27.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018.September
+{
+    public static class Sep27
+    {
+        /*
+         * Title: Reverse Integer
+         * Description: 
+                    Given a 32-bit signed integer, reverse digits of an integer.
+         * Example: Input: 123  Output: 321
+                    Input: -123 Output: -321
+                    Input: 120  Output: 21
+         * Note:
+                Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31, 2^31 − 1]. 
+                For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows
+        */
+        public static int Reverse(int x)
+        {
+            int rev = 0;
+            while (x != 0)
+            {
+                int pop = x % 10;
+                x /= 10;
+                if (rev > int.MaxValue / 10 || (rev == int.MaxValue / 10 && pop > 7)) return 0;
+                if (rev < int.MinValue / 10 || (rev == int.MinValue / 10 && pop < -8)) return 0;
+                rev = rev * 10 + pop;
+            }
+            return rev;
+        }
+        //public static int Reverse(int x)
+        //{
+        //    return (x >= 0 ? 1 : -1) * int.Parse(Math.Abs(x).ToString().Reverse());
+        //}
+    }
+}
