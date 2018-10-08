162f9717dc24a832ecf427165c9bea5585f85dca
姜小骚 <826420016@qq.com>
2018-10-08 22:40:58 +0800

    Oct08 Check In

---
+++ 2018.Test/Oct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using _2018.October;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2018.Test
+{
+    [TestClass]
+    public class Oct
+    {
+        #region Oct-08
+        [TestMethod]
+        public void Oct08Test_One()
+        {
+            Assert.AreEqual(Oct08.RomanToInt("III"), 3);
+            Assert.AreEqual(Oct08.RomanToInt("IV"), 4);
+            Assert.AreEqual(Oct08.RomanToInt("IX"), 9);
+            Assert.AreEqual(Oct08.RomanToInt("LVIII"), 58);
+            Assert.AreEqual(Oct08.RomanToInt("MCMXCIV"), 1994);
+        }
+        #endregion
+    }
+}
---
+++ 2018/October/Oct08.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018.October
+{
+    public static class Oct08
+    {
+        public static int RomanToInt(string value)
+        {
+            int result = 0;
+            for (int i = value.Length - 1; i >=0; i--) 
+            {
+                char t = value[i];
+                switch (t)
+                {
+                    case 'I':
+                        result += result >= 5 ? -1 : 1;
+                        break;
+                    case 'V':
+                        result += 5;
+                        break;
+                    case 'X':
+                        result += result >= 50 ? -10 : 10;
+                        break;
+                    case 'L':
+                        result += 50;
+                        break;
+                    case 'C':
+                        result += result >= 500 ? -100 : 100;
+                        break;
+                    case 'D':
+                        result += 500;
+                        break;
+                    case 'M':
+                        result += 1000;
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
